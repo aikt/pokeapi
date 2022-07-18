09f4df014be8d1a2288ac5f6ba1daa0e30868fd7
System Administrator <root@Ricardos-MacBook-Pro.local>
2022-07-17 23:47:30 -0500

    Upload express.js part 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () => {
   console.log('server is running on port 2022', server.address().port);
